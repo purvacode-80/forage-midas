5b0f437d8a5024f138ef814fa9738d2035b59f94
purvacode-80 <purvaw06@gmail.com>
2026-06-22 21:47:06 +0530

    Task Two Completed - Kafka

---
+++ src/main/java/com/jpmc/midascore/KafkaConsumer.java
@@ -0,0 +1,21 @@
+package com.jpmc.midascore;
+
+import com.jpmc.midascore.foundation.Transaction;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaConsumer {
+
+    @KafkaListener(
+            topics = "${general.kafka-topic}",
+            groupId = "midas-group",
+            properties = {
+                    "spring.json.value.default.type=com.jpmc.midascore.foundation.Transaction"
+            }
+    )
+    public void listen(Transaction transaction) {
+        System.out.println("Transaction received: " + transaction);
+        System.out.println("Amount = " + transaction.getAmount());
+    }
+}
