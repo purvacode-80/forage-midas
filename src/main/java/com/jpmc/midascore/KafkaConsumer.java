aea451cee82a65575d3465e147a7ea2eb2a2c6df
purvacode-80 <purvaw06@gmail.com>
2026-06-23 17:13:34 +0530

    Task 3 Completed - H2 Database

--- src/main/java/com/jpmc/midascore/KafkaConsumer.java
+++ src/main/java/com/jpmc/midascore/KafkaConsumer.java
@@ -1,11 +1,19 @@
 package com.jpmc.midascore;
 
+import com.jpmc.midascore.component.DatabaseConduit;
+import com.jpmc.midascore.entity.UserRecord;
 import com.jpmc.midascore.foundation.Transaction;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class KafkaConsumer {
+
+    private final DatabaseConduit databaseConduit;
+
+    public KafkaConsumer(DatabaseConduit databaseConduit) {
+        this.databaseConduit = databaseConduit;
+    }
 
     @KafkaListener(
             topics = "${general.kafka-topic}",
@@ -15,7 +23,22 @@ public class KafkaConsumer {
             }
     )
     public void listen(Transaction transaction) {
-        System.out.println("Transaction received: " + transaction);
-        System.out.println("Amount = " + transaction.getAmount());
+
+        UserRecord sender = databaseConduit.findById(transaction.getSenderId());
+        UserRecord recipient = databaseConduit.findById(transaction.getRecipientId());
+
+        if (sender == null || recipient == null) {
+            return;
+        }
+
+        if (sender.getBalance() < transaction.getAmount()) {
+            return;
+        }
+
+        sender.setBalance(sender.getBalance() - transaction.getAmount());
+        recipient.setBalance(recipient.getBalance() + transaction.getAmount());
+
+        databaseConduit.save(sender);
+        databaseConduit.save(recipient);
     }
 }
